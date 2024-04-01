23121ababd66ea04564fa5f0d8935a3e68dc4873
Andile-ux <66028486+Andile-ux@users.noreply.github.com>
2024-04-01 18:18:43 +0200

    initialised spring boot app

---
+++ src/main/java/com/Numadic/NumadicApplication.java
@@ -0,0 +1,13 @@
+package com.Numadic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumadicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumadicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Numadic/NumadicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Numadic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumadicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
